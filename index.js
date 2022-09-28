50cca6d131daec4153c8efa4588aa132b35eaf38
akashshawdev <aakashshaw8873@gmail.com>
2022-09-28 20:36:25 +0530

    Complete project

---
+++ index.js
@@ -0,0 +1,70 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () { /*To take mouse clicks*/
+
+    let buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);         /*Clicked button*/
+        buttonAnimation(buttonInnerHTML);
+  });
+}
+
+// To take keystrokes
+document.addEventListener("keydown", function(event){
+        makeSound(event.key)
+        buttonAnimation(event.key)
+})
+
+function makeSound(key) {
+        switch (key) {                    /*variableName*/
+        case "w"                          /*different value*/:
+          let tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+          break;
+  
+        case "a":
+          let tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+          break;
+  
+        case "s":
+          let tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+          break;
+  
+        case "d":
+          let tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+          break;
+  
+        case "j":
+          let snare = new Audio("sounds/snare.mp3");
+          snare.play();
+          break;
+  
+        case "k":
+          let crash = new Audio("sounds/crash.mp3");
+          crash.play();
+          break;
+  
+        case "l":
+          let kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+          break;
+  
+        default:
+          console.log(key);
+          break;
+      }
+}
+
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector("."+ currentKey)
+  activeButton.classList.add("pressed")
+  setTimeout(function(){
+    activeButton.classList.remove("pressed")
+  }, 100)
+}
+
+
